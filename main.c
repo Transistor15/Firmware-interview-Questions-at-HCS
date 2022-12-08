f2a09e5f35ab382aa316f3a64c856c1f52a27d97
Transistor15 <cephasmorgans@gimail.com>
2022-12-08 19:14:08 +0200

    memory allocation using mallac or free

--- main.c
+++ main.c
@@ -1,30 +1,23 @@
 #include <stdio.h>
+#include <malloc.h>
 #include "stddef.h"
-struct Node{
-    unsigned long num;
-    struct Node *next;
-};
-//this is a global variable
-extern unsigned long stack_bottom_address;
-struct Node *insert(struct Node *head, unsigned long number);{
-struct Node *previous=head,*current=head;
-//use the top of the heap and grow downward, should be safe
-stack_bottom_address-=(sizeof(struct Node)+sizeof (unsigned long)-1)/sizeof (unsigned long)*sizeof (unsigned long);
-((struct Node*)stack_bottom_address)->num=number;
-if(head==NULL){
-head=stack_bottom_address;
-head->next=NULL;
-return head;
-}
-while (current!=NULL&&number<current->num){
-previous=current;
-current=current->next;
-}
-previous->next= (struct Node *) stack_bottom_address;
-previous->next->next=current;
-return head;
-}
-int main() {
+#include "stdlib.h"
 
+struct Person{
+    char name[100];
+    int year;
+    int day;
+    char address[200];
+};
+
+int main() {
+    struct Person *person=(struct Person *)malloc(sizeof (struct Person)+sizeof (int));
+    if(person==NULL){
+
+        printf("run of heap memory\n");
+        exit(1);
+    }
+    person=(struct Person *)(((char *)person+sizeof (int)-1)/sizeof (int)*sizeof (int));
+    free(person);
     return 0;
 }
