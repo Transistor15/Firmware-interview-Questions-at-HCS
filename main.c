1615eef52a934756a151fb309533f565aa3389a3
Transistor15 <cephasmorgans@gimail.com>
2022-12-08 20:11:18 +0200

    pass the array to a function

--- main.c
+++ main.c
@@ -2,22 +2,9 @@
 #include <malloc.h>
 #include "stddef.h"
 #include "stdlib.h"
-
-struct Person{
-    char name[100];
-    int year;
-    int day;
-    char address[200];
-};
-
+void _printArray(int the_Array_[],int sizeOfArray);
 int main() {
-    struct Person *person=(struct Person *)malloc(sizeof (struct Person)+sizeof (int));
-    if(person==NULL){
-
-        printf("run of heap memory\n");
-        exit(1);
-    }
-    person=(struct Person *)(((char *)person+sizeof (int)-1)/sizeof (int)*sizeof (int));
-    free(person);
+int tom[3]={1,2,3};
+    _printArray(tom,3);
     return 0;
 }
