988430677e2d3e50ac3abf73b48ce22442f686e8
Transistor15 <cephasmorgans@gimail.com>
2022-12-15 16:36:49 +0200

    Advanced of implementation of a linked list

--- main.c
+++ main.c
@@ -1,40 +1,45 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <stdbool.h>
 
-// Define a structure for the nodes of the linked list
+struct person {
+    char *name;
+    int age;
+    bool isMarried;
+    bool isEmployed;
+    float Net_worth;
+};
+
 struct node {
-    int data;
+    struct person data;
     struct node *next;
 };
 
+void update_person(struct person *p, int new_age) {
+    // Update the age field of the structure pointed to by p
+    p->age = new_age;
+}
+
 int main(void) {
-    // Create the head node of the linked list and initialize its fields
-    struct node *head = malloc(sizeof(struct node));
-    head->data = 10;
-    head->next = NULL;
+    // Define a structure variable and initialize its fields
+    struct person Person = { "Cephas Kalembo", 29, false, false, 100 };
 
-    // Create the second node of the linked list and initialize its fields
-    struct node *second = malloc(sizeof(struct node));
-    second->data = 20;
-    second->next = NULL;
+    // Create a linked list node and initialize its fields
+    struct node person_node = { Person, NULL };
 
-    // Link the head and second nodes together
-    head->next = second;
+    // Declare a pointer to a structure and initialize it with the address of the
+    // linked list node
+    struct node *ptr_to_node = &person_node;
 
-    // Create the third node of the linked list and initialize its fields
-    struct node *third = malloc(sizeof(struct node));
-    third->data = 30;
-    third->next = NULL;
+    // Access the fields of the linked list node using the pointer
+    printf("Name: %s\n", ptr_to_node->data.name);
+    printf("Age: %d\n", ptr_to_node->data.age);
 
-    // Link the second and third nodes together
-    second->next = third;
+    // Call the update_person function, passing a pointer to the person data
+    // in the linked list node and a new age value
+    update_person(&ptr_to_node->data, 30);
 
-    // Print the linked list
-    struct node *current = head;
-    while (current != NULL) {
-        printf("%d\n", current->data);
-        current = current->next;
-    }
+    // Print the updated age value
+    printf("Updated age: %d\n", ptr_to_node->data.age);
 
     return 0;
 }
