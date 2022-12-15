29b7f69d679b13945b3fa5eae237117e1ef7edb9
Transistor15 <cephasmorgans@gimail.com>
2022-12-15 15:42:18 +0200

    using pointers to access the member of the structure

--- main.c
+++ main.c
@@ -1,10 +1,45 @@
 #include <stdio.h>
-#include <malloc.h>
-#include "stddef.h"
-#include "stdlib.h"
-void _printArray(int the_Array_[],int sizeOfArray);
-int main() {
-int tom[3]={1,2,3};
-    _printArray(tom,3);
+#include <stdbool.h>
+
+struct person {
+    char *name;
+    int age;
+    bool isMarried;
+    bool isEmployed;
+    float Net_worth;
+
+};
+
+int main(void) {
+    // Define a structure variable and initialize its fields
+
+    struct person Person = { "Cephas Kalembo ", 29 ,1,1, 100};
+
+    // Declare a pointer to a structure and initialize it with the address of the
+    // structure variable
+    struct person *ptr_to_Struct_Person = &Person;
+
+    // Access the fields of the structure using the pointer
+
+    if(ptr_to_Struct_Person->isMarried && ptr_to_Struct_Person->isEmployed)
+    {
+        printf("my name is %s",ptr_to_Struct_Person->name);
+        printf("i'm %d",ptr_to_Struct_Person->age);
+        printf_s(" years old,");
+        printf(" my unit Net worth is %f",ptr_to_Struct_Person->Net_worth);
+        printf(" , I'm employed working for HERO CONTROL SYSTEMS LLC");
+        printf(" and I'm married with a Son\n");
+    }
+   else
+    {
+        printf("my name is %s",ptr_to_Struct_Person->name);
+        printf("i'm %d",ptr_to_Struct_Person->age);
+        printf_s(" years old");
+        printf(" my unit Net worth is %f",ptr_to_Struct_Person->Net_worth);
+        printf(" , I'm unemployed");
+        printf(" and unmarried\n");
+    }
+if (ptr_to_Struct_Person->isEmployed)
+{}
     return 0;
 }
